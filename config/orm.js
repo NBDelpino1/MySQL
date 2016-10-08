1fac1a15fbc42d1e4ba433410d8b63385c5fe2b0
Nicholas Delpino <Nicky@Nicholas-MacBook-Pro.local>
2016-10-08 16:18:06 -0400

    added ORM object that will execute the nessesary MySQL commands in the controllers

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,30 @@
+// Import (require) `connection.js` into `orm.js`
 var connection = require('./connection.js');
+
+// ORM object, will execute the necessary MySQL commands in the controllers
+var orm = {
+  selectAll: function(cb) {
+    var command = 'SELECT * FROM burgers';
+    connection.query(command, function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  insertOne: function(tableInput, nameInput, cb) { 
+    var command = "INSERT INTO " + tableInput + " (burger_name) VALUES (?)";
+    connection.query(command, [nameInput], function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  updateOne: function(tableInput, idInput, cb) {
+    var command = "UPDATE " + tableInput + " SET updateOne = 1  WHERE burger_name = ?";
+    connection.query(command, [idInput], function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+
+}
+
+module.exports = orm;
